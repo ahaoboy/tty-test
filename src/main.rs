ff8c9c6ee139041462fbf50c5ef473d176f2fe07
ahaoboy <504595380@qq.com>
2024-11-18 13:54:21 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,36 @@
+use std::io::IsTerminal;
+
 fn isatty(fd: i32) -> bool {
     unsafe { libc::isatty(fd) != 0 }
 }
 
+fn is_terminal(fd: i32) -> bool {
+    match fd {
+        0 => std::io::stdin().is_terminal(),
+        1 => std::io::stdout().is_terminal(),
+        2 => std::io::stderr().is_terminal(),
+        _ => false,
+    }
+}
+
 fn main() {
     let s: String = (0..5).map(|i| (isatty(i) as usize).to_string()).collect();
-    println!("{s}")
+    println!("{s}");
+    let s: String = (0..5).map(|i| (is_terminal(i) as usize).to_string()).collect();
+    println!("{s}");
 }
 
 #[cfg(test)]
 mod test {
-    use crate::isatty;
+    use crate::{is_terminal, isatty};
 
     #[test]
     fn test() {
         let s: String = (0..5).map(|i| (isatty(i) as usize).to_string()).collect();
+        println!("{s}");
+        assert_eq!("11100", s);
+
+        let s: String = (0..5).map(|i| (is_terminal(i) as usize).to_string()).collect();
         println!("{s}");
         assert_eq!("11100", s);
     }
